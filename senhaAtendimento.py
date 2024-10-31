f505aa89208a0510ec27076e7c83fd559e3b7ff0
pablohcff <126625031+pablohcff@users.noreply.github.com>
2024-10-31 18:22:27 -0300

    Update senhaAtendimento.py
    Deixei a exibição na tela mais limpa para o usuário.

--- senhaAtendimento.py
+++ senhaAtendimento.py
@@ -5,8 +5,11 @@ n_senhaNormal = 0
 n_senhaPrioridade = 0
 
 def verificar_ordem():
-    print("Senhas ATENDIMENTO NORMAL: ")
+    print("\n Senhas ATENDIMENTO NORMAL: ")
     print(f"{ordem_senhasNormal}")
+    print("\n \n")
+    print("\n Senhas ATENDIMENTO PRIORIDADE: ")
+    print(f"{ordem_senhasPrioridade}\n")
 
 def senha_normal():
     global n_senhaNormal
@@ -15,24 +18,25 @@ def senha_normal():
     ordem_senhasNormal.append({
         "Nº": n_senhaNormal,
     })
-    print(f"Senha: Nº{n_senhaNormal}")
+    print(f"\nSenha: Nº{n_senhaNormal}")
 
-    retornar_menu = input("Pressione enter pra voltar ao menu principal: ")
-
+    retornar_menu = input("\nPressione enter pra voltar ao menu principal: ")
     return main_menu()
 
 def senha_prioridade():
     global n_senhaPrioridade
 
     n_senhaPrioridade += 1
-    ordem_senhasPrioridade.append(n_senhaPrioridade)
-    print(f"Senha: Pº{n_senhaPrioridade}\n")
+    ordem_senhasPrioridade.append({
+        "Nº": n_senhaPrioridade,
+    })
+    print(f"\nSenha: Pº{n_senhaPrioridade}")
 
-    retornar_menu = input("Pressione enter pra voltar ao menu principal: ")
-
+    retornar_menu = input("\nPressione enter pra voltar ao menu principal: ")
     return main_menu()
 
 def main_menu():
+
     print("\n *** BEM-VINDO AO GERADOR DE SENHA DE ATENDIMENTO *** \n")
     print("Por favor, selecione a categoria que você se enquadra: \n")
     print("1. ATENDIMENTO NORMAL")
@@ -46,7 +50,8 @@ def main_menu():
         senha_prioridade()
     elif opcao_menu == 3:
         verificar_ordem()
-    
+    else:
+        print("Opção inválida.")
 
 while True:
     main_menu()
